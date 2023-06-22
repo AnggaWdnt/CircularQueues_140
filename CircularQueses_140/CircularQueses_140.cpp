55716dfb3025713f32ec9b8859a1200ef1bcf2f7
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-06-22 15:09:07 +0700

    tambahan perintah else dan if di void insert

--- CircularQueses_140/CircularQueses_140.cpp
+++ CircularQueses_140/CircularQueses_140.cpp
@@ -18,10 +18,23 @@ public:
 		cout << endl;
 
 		// Cek apakah antrian penuh
-		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR = +1)) {
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR = + 1)) {
 			cout << "\nQueues overflow\n";
 			return;
 		}
 		// cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
 	}
-};
+
+	
